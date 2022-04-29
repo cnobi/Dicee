0cd3bc3130f53d9def20a2133107a411b4eecdf6
Susanshr <shr.susan7@gmail.com>
2022-04-29 13:48:13 +0545

    Decreased the font-size of h1 to 4.5rem

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ else if ( randomNumber2 > randomNumber1 ) {
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 else {
-    document.querySelector("h1").innerHTML = "Draw! Roll the dice again!";
+    document.querySelector("h1").innerHTML = "Draw! Roll the dice again.";
 }
